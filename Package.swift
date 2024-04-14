f180be4244d3ab3b0cd7e23f68c0d8eb7ab130f5
cstultz <kalub92@gmail.com>
2024-04-13 21:58:38 -0700

    Fixes

--- Package.swift
+++ Package.swift
@@ -9,13 +9,66 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SamplePackageB",
-            targets: ["SamplePackageB"]),
+            targets: ["SamplePackageB"]
+        ),
+        .library(
+            name: "3rdPartyResources",
+            targets: ["3rdPartyResources"]
+        ),
+        .library(
+            name: "3rdPartyAdapter",
+            targets: ["3rdPartyAdapter"]
+        )
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SamplePackageB"),
+        .target(
+            name: "3rdPartyAdapter",
+            dependencies: [
+                "SamplePackageB",
+                "3rdPartyResources"
+            ],
+            linkerSettings: [
+                .linkedFramework("AuthenticationServices"),
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("LocalAuthentication")
+            ]
+        ),
+        .target(
+            name: "3rdPartyResources",
+            dependencies: [
+                .target(
+                    name: "OneSignal",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .target(
+                    name: "OneSignalCore",
+                    condition: .when(platforms: [.iOS])
+                ),
+                .target(
+                    name: "OneSignalExtension",
+                    condition: .when(platforms: [.iOS])
+                )
+            ],
+            resources: [
+                .copy("Resources/3pa.sdk.bundle"),
+            ]
+        ),
+        .binaryTarget(
+            name: "OneSignal",
+            path: "Frameworks/OneSignal.xcframework"
+        ),
+        .binaryTarget(
+            name: "OneSignalCore",
+            path: "Frameworks/OneSignalCore.xcframework"
+        ),
+        .binaryTarget(
+            name: "OneSignalExtension",
+            path: "Frameworks/OneSignalExtension.xcframework"
+        ),
         .testTarget(
             name: "SamplePackageBTests",
             dependencies: ["SamplePackageB"]),
